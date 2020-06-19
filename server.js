279155f168b862556ca1cd92b4f775bb03e4f580
Lillian Namusisi <lilliannamusisi@gmail.com>
2020-06-19 03:51:37 +0300

    changed server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+var app = require('express')();
+var server = require('http').Server(app);
+server.listen(80);
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/timelines'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/timelines/index.html'));
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
+console.log('Express server started on port %s', server.address().port);
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 3000;
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+app.listen(port, function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
